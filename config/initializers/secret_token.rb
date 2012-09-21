bd062d6838e58af85674678ec2f3fd0586d485ca
Liz Bennett <liz@bennettworld.org>
2012-09-21 13:08:07 -0400

    Initial Commit. Adding all Rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ObieTaxi::Application.config.secret_token = 'b8e8521e327706f8177cdda2fc1b268185af7c6aebe1b55c252c7fb14f6c6dc1b6abc58214ff31651d587c71cd49e8454b0b724f60a9ac80b665406d9a4b749a'
